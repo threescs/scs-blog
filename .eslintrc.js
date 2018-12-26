72c41ceca3b9b1d2feeb1a50b9a911c81fd3a29a
尚成帅 <95513113@qq.com>
2018-12-26 17:06:25 +0800

    feat: Home页添加banner && 修正eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,40 +16,56 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "no-tabs":"off",
-        'linebreak-style':'off',
-        'max-len': 'off',
-        'quotes':'off',
-        'eqeqeq':'off',
-        'no-console': 'off',
-        'no-alert': 'off',
-        'no-bitwise': 'off',
-        'no-empty': 'off',
-        'no-underscore-dangle': 'off',
-        'consistent-return': 'off',
-        'no-unused-vars': 'off',
-        'import/prefer-default-export': 'off',
-        'no-unused-expressions': 'off',
-        'no-plusplus': 'off',
-        'func-names':'off',
-        'no-restricted-syntax': 'off',
-        'one-var':'off',
-        'no-prototype-builtins':'off',
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+      "indent": [2, 4],
+      "global-require": 0,
+      "new-cap": 0,
+      "func-names": 0,
+      "consistent-return": 0,
+      "max-len": 0,
+      "require-yield": 0,
+      "guard-for-in": 0,
+      "object-curly-newline": 0,
+
+      "import/no-extraneous-dependencies": 0,
+      "import/no-unresolved": 0,
+      "import/no-commonjs": 0,
+      "import/no-nodejs-modules": 0,
+      "import/no-mutable-exports": 0,
+      "import/extensions": [
+          2,
+          {
+              "js": "never",
+              "jsx": "never"
+          }
+      ],
+      "import/no-dynamic-require": 0,
+
+      "no-console": 0,
+      "no-plusplus": 0,
+      "no-param-reassign": 0,
+      "no-restricted-syntax": [2, "WithStatement"],
+      "no-invalid-this": 0,
+      "no-underscore-dangle": 0,
+      "no-return-assign": 0,
+      "no-await-in-loop": 0,
+      "no-mixed-operators": 0,
+      "no-use-before-define": 0,
+
+      "react/jsx-indent": [2, 4],
+      "react/jsx-indent-props": [2, 4],
+      "react/jsx-filename-extension": 0,
+      "react/prefer-stateless-function": 0,
+      "react/require-default-props": 0,
+      "react/no-danger": 0,
+      "react/no-array-index-key": 0,
+      "react/no-multi-comp": 0,
+      "react/forbid-prop-types": 0,
+      "react/jsx-no-bind": 0,
+      "react/no-did-update-set-state": 0,
+      "jsx-a11y/img-has-alt": 0,
+      "jsx-a11y/no-static-element-interactions": 0,
+      "jsx-a11y/no-noninteractive-element-interactions": 0,
+      "jsx-a11y/alt-text": 0,
+      "jsx-a11y/click-events-have-key-events": 0
     }
 };
---
+++ app/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import IndexApp from './containers';
+import './reset.css';
+
+const div = document.createElement('div');
+div.setAttribute('id', 'app');
+document.body.appendChild(div);
+
+const mountNode = document.getElementById('app');
+
+render(
+    <AppContainer>
+        <IndexApp />
+    </AppContainer>,
+    mountNode,
+);
+
+if (module.hot && process.env.NODE_ENV !== 'production') {
+    module.hot.accept();
+}
