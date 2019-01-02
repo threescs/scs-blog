58b2c5badfd6bea4afe7d51f5e7fef85c67d9e41
尚成帅 <95513113@qq.com>
2019-01-02 18:38:55 +0800

    add

--- models/article.js
+++ models/article.js
@@ -2,6 +2,16 @@
  * Article model
  */
  const articleSchema = require('../schemas/article');
+ const { mongoose } = require('../core/mongodb.js');
  const autoIncrement = require('mongoose-auto-increment');
 
-//  
+//  自增ID插件配置
+articleSchema.plugin(autoIncrement.plugin, {
+  model: 'Article',
+  fieId: 'id',
+  startAt: 1,
+  incrementBy: 1
+});
+
+// 文章模型
+module.exports = mongoose.model('Article', articleSchema);
